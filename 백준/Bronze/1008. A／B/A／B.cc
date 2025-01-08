410fb53e58782d8003461be46f70917100b82315
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-01-08 20:01:48 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double A, B;
+    cin >> A >> B;
+    cout << setprecision(10) << A / B << endl;
+    return 0;
+}
