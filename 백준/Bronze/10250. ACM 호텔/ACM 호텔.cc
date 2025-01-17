4131f44d38be18d35b7c9d0b1a5e44923dc98b44
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-01-17 18:51:20 +0900

    [Bronze III] Title: ACM 호텔, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int H, W, N;
+        cin >> H >> W >> N;
+        int floor = (N - 1) % H +1;
+        int room = (N - 1) / H + 1;
+        cout << floor * 100 + room << endl;
+    }
+    return 0;
+}
