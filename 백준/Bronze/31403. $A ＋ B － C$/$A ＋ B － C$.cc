1472d5e9565d58ca7a8d505001f872cb4c8e324e
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-01-17 17:01:50 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string a, b, c;
+    cin >> a >> b >> c;
+    cout << stoi(a) + stoi(b) - stoi(c) << endl;
+    cout << stoi(a + b) - stoi(c) << endl;
+    return 0;
+}
