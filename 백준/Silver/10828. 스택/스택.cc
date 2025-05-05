f3b476bc0e3d2145dd6903b09b30f7574bf04c66
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-05-05 17:43:13 +0900

    [Silver IV] Title: 스택, Time: 208 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main(){
+    stack<int> s;
+    int N;
+    cin >> N;
+    while(N--){
+        string command;
+        cin >> command;
+        if(command == "push"){
+            int x;
+            cin >> x;
+            s.push(x);
+        }else if(command == "pop"){
+            if(s.empty()){
+                cout << -1 << "\n";
+            }else{
+                cout << s.top() << "\n";
+                s.pop();
+            }
+        }else if(command == "size"){
+            cout << s.size() << "\n";
+        }else if(command == "empty"){
+            if(s.empty()){
+                cout << 1 << "\n";
+            }else{
+                cout << 0 << "\n";
+            }
+        }else if(command == "top"){
+            if(s.empty()){
+                cout << -1 << "\n";
+            }else{
+                cout << s.top() << "\n";
+            }
+        }
+    }
+    return 0;
+}
