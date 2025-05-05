b33afcd0a45ab476974199356daa134329bc00ed
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-05-05 17:54:25 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    queue<int> q;
+    int N;
+    cin >> N;
+    while(N--){
+        string s;
+        cin >> s;
+        if(s == "push"){
+            int x;
+            cin >> x;
+            q.push(x);
+        }else if(s == "pop"){
+            if(q.empty()){
+                cout << -1 << "\n";
+            }else{
+                cout << q.front() << "\n";
+                q.pop();
+            }
+        }else if(s == "size"){
+            cout << q.size() << "\n";
+        }else if(s == "empty"){
+            if(q.empty()){
+                cout << "1" << "\n";
+            }else{
+                cout << "0" << "\n";
+            }
+        }else if(s == "front"){
+            if(q.empty()){
+                cout << -1 << "\n";
+            }else{
+                cout << q.front() << "\n";
+            }
+        }else if(s == "back"){
+            if(q.empty()){
+                cout << -1 <<"\n";
+            }else{
+                cout << q.back() << "\n";
+            }
+        }
+    }
+    return 0;
+}
