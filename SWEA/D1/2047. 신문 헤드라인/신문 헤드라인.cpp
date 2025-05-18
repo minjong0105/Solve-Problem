9f0ac22ce1087f384633cce85c1a26c99f52426c
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-05-19 00:54:30 +0900

    [D1] Title: 신문 헤드라인, Time: 4 ms, Memory: 396 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    for(int i = 0; i < s.length(); i++){
+        s[i] = toupper(s[i]);
+    }
+    cout << s << '\n';
+    return 0;
+}
