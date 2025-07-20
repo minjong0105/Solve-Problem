0aba97ee089344e6303efcd0149aa692b4f897c7
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-07-20 14:56:15 +0900

    [D1] Title: 1대1 가위바위보, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.cpp
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
-using namespace std;
+import java.util.Scanner;
 
-int main(){
-    int A, B;
-    cin >> A >> B;
-    if(A == 1){
-        if(B == 2) cout << "B";
-        if(B == 3) cout << "A";
-    }else if(A == 2){
-        if(B == 1) cout << "A";
-        if(B == 3) cout << "B";
-    }else{
-        if(B == 1) cout << "B";
-        if(B == 2) cout << "A";
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if ((A == 1 && B == 3) || (A == 2 && B == 1) || (A == 3 && B == 2)) {
+            System.out.println("A");
+        } else {
+            System.out.println("B");
+        }
     }
 }
