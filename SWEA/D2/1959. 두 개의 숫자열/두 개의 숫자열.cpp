b9487825084d76baf7a8aee6343780174902ae4c
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-07-20 21:02:14 +0900

    [D2] Title: 두 개의 숫자열, Time: 5 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.cpp
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.cpp
@@ -1,52 +1,46 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+import java.util.Scanner;
 
-int main(){
-    int T;
-    cin >> T;
-    for(int t = 1; t <= T; t++){
-        int N, M;
-        cin >> N >> M;
-        vector<int> A(N);
-        for(int i = 0; i < N; i++){
-            cin >> A[i];
-        }
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
 
-        vector<int> B(M);
-        for(int j = 0; j < M; j++){
-            cin >> B[j];
-        }
-        
-        int max = 0;
-        
-        if(N == M){
-            int sum = 0;
-            for(int i = 0; i < N; i++){
-                sum += A[i] * B[i];
+        for (int t = 1; t <= T; t++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+            int[] A = new int[N];
+            int[] B = new int[M];
+
+            for (int i = 0; i < N; i++) {
+                A[i] = sc.nextInt();
             }
-            max = sum;
-        }
-        
-        if(N < M){
-            for(int i = 0; i < M - N + 1; i++){
-            int sum = 0;
-                for(int j = 0; j < N; j++){
-                    sum += A[j] * B[j + i];
+
+            for (int i = 0; i < M; i++) {
+                B[i] = sc.nextInt();
+            }
+
+            int max = Integer.MIN_VALUE;
+
+            if (N <= M) {
+                for (int i = 0; i <= M - N; i++) {
+                    int sum = 0;
+                    for (int j = 0; j < N; j++) {
+                        sum += A[j] * B[i + j];
+                    }
+                    max = Math.max(max, sum);
                 }
-                if(max < sum) max = sum; 
-            }
-        }
-        if(N > M){
-            for(int i = 0; i < N - M + 1; i++){
-            int sum = 0;
-                for(int j = 0; j < M; j++){
-                    sum += A[j + i] * B[j];
+            } else {
+                for (int i = 0; i <= N - M; i++) {
+                    int sum = 0;
+                    for (int j = 0; j < M; j++) {
+                        sum += A[i + j] * B[j];
+                    }
+                    max = Math.max(max, sum);
                 }
-                if(max < sum) max = sum; 
             }
+
+            System.out.println("#" + t + " " + max);
         }
-        cout << "#" << t << ' ' << max << '\n';
     }
-    return 0;
 }
