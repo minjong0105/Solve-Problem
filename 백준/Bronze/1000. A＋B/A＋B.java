7015ff94e065ba2fca671f0f8eed7ba361be4e79
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-08-19 22:47:49 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int A = Integer.parseInt(s[0]);
+        int B = Integer.parseInt(s[1]);
+        System.out.println(A + B);
+    }
+}
