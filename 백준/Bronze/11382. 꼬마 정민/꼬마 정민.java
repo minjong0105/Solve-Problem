bc5510ed216c4c0b467ef1c213903f6f0951ed48
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-08-27 00:04:54 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+        long sum = A + B + C;
+        System.out.println(sum);
+    }
+}
