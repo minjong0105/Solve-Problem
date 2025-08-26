7bb7d9e131b8a108bba2ae518cfbb5a40773e592
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-08-26 20:35:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 85 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -22,18 +22,18 @@ public class Solution {
             int[] dp = new int[13];
 
             for(int i = 1; i <= 12; i++){
-                int cost1 = dp[i - 1] + day * plan[i];
-                int cost2 = dp[i - 1] + month;
-                int cost3 = Integer.MAX_VALUE;
-
+                int cost1 = dp[i - 1] + day * plan[i];      //1일 이용권 비용
+                int cost2 = dp[i - 1] + month;              //1달 이용권 비용
+                int cost3 = Integer.MAX_VALUE;              //3달 이용권 비용
                 if(i >= 3){
+                    //1, 2월에는 3달 이용권을 사용하면 1월부터 시작해야 되니까 dp[0] 사용
                     cost3 = dp[i - 3] + quarter;
                 }
 
-                dp[i] = Math.min(cost1, Math.min(cost2, cost3));
+                dp[i] = Math.min(cost1, Math.min(cost2, cost3));    //3개 중 제일 싼거
             }
 
-            int ans = Math.min(dp[12], year);
+            int ans = Math.min(dp[12], year);       //1년 이용권이랑 비교해서 더 싼거
 
             System.out.println("#" + tc + " " + ans);
         }
