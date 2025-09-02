1edf64ef281891ae436aaa3bb494946bd41ed9c1
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-09-02 15:27:22 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 712 ms, Memory: 94536 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,20 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        int[] plus = new int[N];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		boolean[] plusArr = new boolean[1000001];
+		boolean[] minusArr = new boolean[1000001];
+		
+		for (int i = 0; i < N; i++) {
+			int input = Integer.parseInt(br.readLine());
+			if (input > 0) {
+				plusArr[input] = true;
+			} else {
+				input *= -1;
+				minusArr[input] = true;
+			}
+		}
+				
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 1000000; i >= 0; i--) {
+			if (minusArr[i]) {
+				sb.append((i * -1)).append("\n");
+			}
+		}
+		for (int i = 0; i < 1000001; i++) {
+			if (plusArr[i]) {
+				sb.append(i).append("\n");
+			}
+		}
+		
+		System.out.println(sb);
+	}
 
-        for (int i = 0; i < N; i++) {
-            plus[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(plus);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(plus[i]).append("\n");
-        }
-        System.out.println(sb);
-    }
 }
+
