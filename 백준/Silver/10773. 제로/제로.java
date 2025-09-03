cc2e9b2b5b9bda0bbb55bd562c537b1bc05df16f
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-09-03 11:43:40 +0900

    [Silver IV] Title: 제로, Time: 208 ms, Memory: 23940 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        int ans = 0;
+        for (int i : stack) {
+            ans += i;
+        }
+        System.out.println(ans);
+    }
+}
