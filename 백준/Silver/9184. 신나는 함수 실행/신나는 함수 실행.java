03c9e79c78f2848de3286156bfd746a552fb4d1b
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-09-04 14:27:47 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 256 ms, Memory: 26140 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[][][] dp = new int[21][21][21];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == -1 && b == -1 && c == -1) {
+                break;
+            }
+
+            int ans = w(a, b, c);
+            sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ")
+                    .append(ans).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+
+    public static int w(int a, int b, int c) {
+        if (inRange(a, b, c) && dp[a][b][c] != 0) {
+            return dp[a][b][c];
+        }
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        } else if (a > 20 || b > 20 || c > 20) {
+            return dp[20][20][20] = w(20, 20, 20);
+        } else if (a < b && b < c) {
+            return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        } else {
+            return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        }
+    }
+
+    public static boolean inRange(int a, int b, int c) {
+        return a >= 0 && a <= 20 && b >= 0 && b <= 20 && c >= 0 && c <= 20;
+    }
+}
