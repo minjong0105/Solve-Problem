2c4fb0ce6a57771a27e8b8f505e3d7811e03b8be
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-09-24 09:02:46 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 116 ms, Memory: 15892 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String id = br.readLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + id + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
