254b889ca8a1226fd84be6038359f113bb2443ad
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-10-01 09:25:47 +0900

    [Silver I] Title: 경로 찾기, Time: 152 ms, Memory: 15336 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] g = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                g[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (g[i][k] == 1 && g[k][j] == 1)
+                        g[i][j] = 1;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(g[i][j]).append(' ');
+            }
+            sb.append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
