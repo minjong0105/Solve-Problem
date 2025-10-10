96b7ea5666e5c3c5aa122403f84859bc2e4715f1
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-10-10 17:29:40 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 484 ms, Memory: 145108 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static char[][] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new char[N][2 * N - 1];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2 * N - 1; j++) {
+                arr[i][j] = ' ';
+            }
+        }
+
+        fx(N, 0, N - 1); // 24, 47, 24
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2 * N - 1; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void fx(int n, int r, int c) {
+        if (n == 3) {
+            arr[r][c] = '*';
+            arr[r + 1][c - 1] = '*';
+            arr[r + 1][c + 1] = '*';
+            arr[r + 2][c - 2] = '*';
+            arr[r + 2][c - 1] = '*';
+            arr[r + 2][c] = '*';
+            arr[r + 2][c + 1] = '*';
+            arr[r + 2][c + 2] = '*';
+            return;
+        }
+
+        int half = n / 2;
+        fx(n / 2, r, c);
+        fx(n / 2, r + half, c - half);
+        fx(n / 2, r + half, c + half);
+    }
+}
