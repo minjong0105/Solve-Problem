bf86269b56abfe82d69c19db29e2837dc7cafc21
minjong0105 <66308879+minjong0105@users.noreply.github.com>
2025-12-20 23:33:08 +0900

    [Bronze III] Title: 행복, Time: 112 ms, Memory: 14348 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15969. 행복/행복.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < N; i++) {
+            int score = Integer.parseInt(st.nextToken());
+            
+            max = Math.max(max, score);
+            
+            min = Math.min(min, score);
+        }
+        
+        System.out.println(max - min);
+    }
+}
