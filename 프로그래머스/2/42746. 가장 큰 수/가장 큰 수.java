c660e704f6fb6b70110f7575aafcdc3ff534c417
김민종 <66308879+minjong0105@users.noreply.github.com>
2026-04-29 21:58:11 +0900

    [level 2] Title: 가장 큰 수, Time: 181.92 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+        
+        for(int i = 0; i < numbers.length; i++) {
+            arr[i] = Integer.toString(numbers[i]);
+        }
+        
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        if(arr[0].equals("0")){
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String str : arr) {
+            sb.append(str);
+        }
+        
+        
+        return sb.toString();
+    }
+}
