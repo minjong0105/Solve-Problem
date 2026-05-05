c7feb167accde9cec3a5bb07f437e509fd069845
김민종 <66308879+minjong0105@users.noreply.github.com>
2026-05-05 22:46:48 +0900

    [level 2] Title: 올바른 괄호, Time: 12.79 ms, Memory: 55.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if(c == '(') {
+                stack.push(c);
+            }else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
