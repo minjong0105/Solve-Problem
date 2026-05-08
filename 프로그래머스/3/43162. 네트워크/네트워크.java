363188ef002315003a824ae29296fb1b6d90ad24
김민종 <66308879+minjong0105@users.noreply.github.com>
2026-05-08 22:27:59 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(i, n, computers, visited);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private void dfs(int i, int n, int[][] computers, boolean[] visited) {
+        visited[i] = true;
+        
+        for(int j = 0; j < n; j++){
+            if(i != j && computers[i][j] == 1 && !visited[j]) {
+                dfs(j, n, computers, visited);
+            }
+        }
+    }
+}
