147a0c933f6c8b808af346da4b3995c2bff3f023
김민종 <66308879+minjong0105@users.noreply.github.com>
2026-09-02 20:57:08 +0900

    [level 2] Title: 힌트 스테이지, Time: 2.89 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
@@ -1,37 +1,39 @@
 class Solution {
-    int[][] cost, hint;
     int ans = Integer.MAX_VALUE;
-    
+    int[][] cost, hint;
+    int len;
     public int solution(int[][] cost, int[][] hint) {
         this.cost = cost;
         this.hint = hint;
-        dfs(0, 0, new int[cost.length + 1]);
+        this.len = cost.length;
+        
+        dfs(0, 0, new int[len]);
         return ans;
     }
     
-    private void dfs(int idx, int spend, int[] have) {
+    private void dfs(int stage, int total, int[] have) {
+        if (total >= ans) return;
         
-        if (idx == hint.length) {
-            int total = spend;
-            for (int i = 1; i <= cost.length; i ++) {
-                total += cost[i - 1][Math.min(have[i], cost.length - 1)];
-            }
+        if (stage == len) {
             ans = Math.min(ans, total);
             return;
         }
         
-        // 안 사는거
-        dfs(idx + 1, spend, have);
+        int use = Math.min(have[stage], len - 1);
+        int solve = cost[stage][use];
         
-        // 사는거
-        for (int i = 1; i < hint[idx].length; i++) {
-            have[hint[idx][i]]++;
-        }
+        dfs(stage + 1, total + solve, have);
         
-        dfs(idx + 1, spend + hint[idx][0], have);
-        
-        for (int i = 1; i < hint[idx].length; i++) {
-            have[hint[idx][i]]--;
+        if (stage < len - 1) {
+            for (int i = 1; i < hint[stage].length; i++) {
+                have[hint[stage][i] - 1]++;
+            }
+            
+            dfs(stage + 1, total + solve + hint[stage][0], have);
+            
+            for (int i = 1; i < hint[stage].length; i++) {
+                have[hint[stage][i] - 1]--;
+            }
         }
     }
 }
