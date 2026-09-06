d3fa5cfdcd92cf05d1442504ccd22f803800b1a8
김민종 <66308879+minjong0105@users.noreply.github.com>
2026-09-06 14:33:49 +0900

    [level 3] Title: 단속카메라, Time: 5.49 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 1;
+        
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        
+        int pos = routes[0][1];
+        
+        for (int i = 1; i < routes.length; i++) {
+            if (routes[i][0] <= pos) continue;
+            
+            answer++;
+            pos = routes[i][1];
+        }
+        
+        return answer;
+    }
+}
